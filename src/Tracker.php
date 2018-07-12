b713e483398ea9efcf3f8e5d94a6b6854cbbfda0
Arpan Rank <rankarpan@hotmail.com>
2018-07-12 17:57:41 +0530

    Log Tracker with Sql Query

---
+++ src/Tracker.php
@@ -0,0 +1,86 @@
+<?php
+
+/**
+ * This file is part of the Laravel Visitor Tracker package.
+ *
+ * @author     Arpan Rank <arpan@iconscout.com>
+ * @copyright  2018
+ *
+ * For the full copyright and license information,
+ * please view the LICENSE.md file that was distributed
+ * with this source code.
+ */
+
+namespace Iconscout\Tracker;
+
+use DB;
+use Carbon\Carbon;
+use Webpatser\Uuid\Uuid;
+use Elasticsearch\ClientBuilder;
+
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Cookie;
+use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Facades\Session;
+use Iconscout\Tracker\Drivers\ElasticSearch;
+
+class Tracker
+{
+    /**
+     * @var string
+     */
+    protected $es;
+
+    /**
+     * ElasticSearch constructor.
+     */
+    public function __construct()
+    {
+        $this->es = new ElasticSearch;
+    }
+
+    public function logSqlQuery($sql, $bindings, $time, $connection_name)
+    {
+        $sql_query = htmlentities($sql);
+        $database_name = DB::connection($connection_name)->getDatabaseName();
+
+        foreach ($bindings as $key => $binding) {
+            $bindings[$key] = $this->isBinary($binding) ? base64_encode($binding) : $binding;
+        }
+
+        $cookie = $this->cookieTracker();
+        $log = Cache::tags('tracker')->get($cookie);
+
+        $model = [
+            'id' => Uuid::generate(1, '02:42:ac:14:00:03')->string,
+            'log_id' => $log['id'],
+            'user_id' => $log['user_id'],
+            'cookie_id' => $log['cookie_id'],
+            'sha1' => Uuid::generate(5, $sql_query, Uuid::NS_DNS)->string,
+            'statement' => $sql_query,
+            'bindings' => json_encode($bindings),
+            'time' => $time,
+            'connection' => $database_name,
+            'created_at' => Carbon::now()->toDateTimeString()
+        ];
+
+        $this->es->indexDocument($model, 'sql_queries');
+    }
+
+    public function cookieTracker()
+    {
+        if (! Cookie::has(Config::get('tracker.cookie'))) {
+            $cookie = Uuid::generate(4)->string;
+            Cookie::queue(Cookie::forever(Config::get('tracker.cookie'), $cookie));
+        } else {
+            $cookie = Cookie::get(Config::get('tracker.cookie'));
+        }
+
+        return $cookie;
+    }
+
+    public function isBinary($str)
+    {
+        return preg_match('~[^\x20-\x7E\t\r\n]~', $str) > 0;
+    }
+}
--- src/TrackerServiceProvider.php
+++ src/TrackerServiceProvider.php
@@ -2,7 +2,21 @@
 
 namespace Iconscout\Tracker;
 
+/**
+ * This file is part of the Laravel Visitor Tracker package.
+ *
+ * @author     Arpan Rank <arpan@iconscout.com>
+ * @copyright  2018
+ *
+ * For the full copyright and license information,
+ * please view the LICENSE.md file that was distributed
+ * with this source code.
+ */
+
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Config;
+
+use Elasticsearch\ClientBuilder;
 
 use Iconscout\Tracker\Console\IndexCommand;
 use Iconscout\Tracker\Console\DeleteCommand;
@@ -42,6 +56,26 @@ class TrackerServiceProvider extends ServiceProvider
                 IndexCommand::class,
                 DeleteCommand::class,
             ]);
+        }
+
+        $this->registerSqlQueryLogWatcher();
+    }
+
+    protected function registerSqlQueryLogWatcher()
+    {
+        $tracker = new Tracker;
+
+        $this->client = ClientBuilder::create()->setHosts(Config::get('tracker.elastic.client.hosts', ['localhost:9200']))->build();
+        $this->index = Config::get('tracker.elastic.index', 'laravel_tracker');
+
+        if (class_exists('Illuminate\Database\Events\QueryExecuted')) {
+            $this->app['events']->listen('Illuminate\Database\Events\QueryExecuted', function ($query) use ($tracker) {
+                $tracker->logSqlQuery($query->sql, $query->bindings, $query->time, $query->connectionName);
+            });
+        } else {
+            $this->app['events']->listen('illuminate.query', function ($sql, $bindings, $time, $connection_name) use ($tracker) {
+                $tracker->logSqlQuery($sql, $bindings, $time, $connection_name);
+            });
         }
     }
 
